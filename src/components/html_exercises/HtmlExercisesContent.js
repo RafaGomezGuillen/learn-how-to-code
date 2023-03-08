2e06e05d2e3098dd4f72ab0135074e198e587da8
Rafa PC <gomezra72.a@gmail.com>
2023-03-08 18:32:10 +0000

    empezando html exercises (ejercicio 1)

--- src/components/html_exercises/HtmlExercisesContent.js
+++ src/components/html_exercises/HtmlExercisesContent.js
@@ -6,132 +6,88 @@ function HtmlExercisesContent() {
     <div className="main">
       <h1>HTML EXERCISES</h1>
       <div className="introduction">
-        <p>To start coding HTML, you have to download an IDE.</p>
         <p>
-          An integrated development environment (IDE) is a software application
-          that provides comprehensive facilities to computer programmers for
-          software development. An IDE normally consists of at least a source
-          code editor, build automation tools and a debugger. Definition by{" "}
-          <a
-            className="links"
-            href="https://en.wikipedia.org/wiki/Integrated_development_environment"
-          >
-            {" "}
-            Wikipedia
-          </a>
-          .
+          In this page we are going to see all 15 HTML documents of the fist
+          quarter.
         </p>
         <p>
-          For this time we are going to use Visual Studio Code by Microsoft.
-        </p>
-        <p>
-          Which is the most used IDE according to{" "}
-          <a href="https://survey.stackoverflow.co/2022/" className="links">
-            Stack Overflow 2022 Developer Survey
-          </a>
-          .
-        </p>
-        <p>In this page we are going to see how to create a HTML document.</p>
-      </div>
-      <hr></hr>
-      <div className="content-code">
-        <h2>STEP 1: Download for free VS Code </h2>
-        <p>
-          Download VS Code for free in this page{" "}
-          <a href="https://code.visualstudio.com/Download" className="links">
-            VS Code page
-          </a>
-          .
+          All of the are made by me when I was starting to learn HTML so it
+          could be optimized or better.
         </p>
       </div>
       <hr></hr>
+
       <div className="content-code">
-        <h2>STEP 2: Create the HTML document </h2>
+        <h2>Exercise 1</h2>
         <p>
-          To create a HTML document press ctrl + n (New Text File), or go to
-          File section on VS Code an click New Text File.
+          In this exercise we practiced arbitrary blanks with the &lt;pre&gt;
+          tag.
         </p>
-      </div>
-      <hr></hr>
-      <div className="content-code">
-        <h2>STEP 3: Save the HTML document </h2>
-        <p>
-          To save the HTML document press ctrl + shift + s (Save As...), or go
-          to File section on VS Code an click Save As.
-          <br></br>
-          <br></br>
-          Introduce the name of the document terminated by .html (extension).
-        </p>
-      </div>
-      <hr></hr>
-      <div className="content-code">
-        <h2>STEP 4: Write Some Html</h2>
-        <p>Write or copy the following HTML code into the document created.</p>
         <CodeContainer
           title={"HTML"}
           code={
-            "<!DOCTYPE html>\n<html>\n<title>HTML Tutorial</title>\n<body>\n\n<h1>Hello world!</h1>\n\n</body>\n</html>"
+            "<html>\n<head>\n <title>Arbitrary blanks</title>\n</head>\n<body>\n\n  <h1><strong>Characteristics of the planets</strong></h1>\n  <pre>\n   Name           Relative diameter            Orbital period          Number of moons\n   ---------------------------------------------------------------------------------------\n   Mercury                0,382                       0,24   years                  0\n   Venus                  0,949                       0,62   years                  0\n   Earth                  1                           1      years                  1\n   Mars                   0,532                       1,88   years                  2\n   Jupiter                11,209                     11,86   years                 49\n   Saturn                 9,449                      29,46   years                 52\n   Uranos                 4,007                      84,01   years                 27\n   Neptun                 3,883                     164,80   years                 13\n </pre>\n\n</body>\n</html>"
           }
-          containerHeight={"340px"}
-          insideHeight={"65%"}
+          containerHeight={"610px"}
+          insideHeight={"80%"}
         />
-      </div>
-      <hr></hr>
-      <div className="content-code">
-        <h2>STEP 5: Save the HTML document and open it</h2>
-        <p>
-          To save the HTML document press ctrl + s (Save...), or go to File
-          section on VS Code an click Save.
-          <br></br>
-          <br></br>
-          You can press the option Auto Save wich auto saves the changes you
-          have done in the document automatically for you.
-          <br></br>
-          <br></br>
-          To see the changes open the File Explorer and the document, you will
-          see it in your default browser.
-        </p>
-      </div>
-      <hr></hr>
-      <div className="content-code">
-        <h2>VS Code Extensions</h2>
-        <p>
-          I recommend you these extensions for VS Code, it will make easier the
-          proccess on writing HTML.
-        </p>
-        <table>
-          <tr>
-            <td>Extension</td>
-            <td>Description</td>
-          </tr>
-          <tr>
-            <td>Auto Rename Tag</td>
-            <td>
-              Automatically rename paired HTML/XML tag, same as Visual Studio
-              IDE does.
-            </td>
-          </tr>
-          <tr>
-            <td>Live Server</td>
-            <td>
-              Launch a development local Server with live reload feature for
-              static & dynamic pages.
-            </td>
-          </tr>
-          <tr>
-            <td>Lorem ipsum</td>
-            <td>Generates and inserts lorem ipsum text.</td>
-          </tr>
-          <tr>
-            <td>Prettier</td>
-            <td>
-              Prettier is an opinionated code formatter. It enforces a
-              consistent style by parsing your code and re-printing it with its
-              own rules that take the maximum line length into account, wrapping
-              code when necessary.
-            </td>
-          </tr>
-        </table>
+        <div className="examples">
+          <h1>
+            <strong>Characteristics of the planets</strong>
+          </h1>
+
+          <pre>
+            Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Relative
+            diameter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Orbital
+            period&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp; Number of moons <br></br>
+            ---------------------------------------------------------------------------------------
+            <br></br>
+            Mercury&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0,382&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0,24
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0<br></br>
+            Venus&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0,949&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0,62
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0 <br></br>
+            Earth&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            1
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            1<br></br>
+            Mars&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0,532&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            1,88
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            2<br></br>
+            Jupiter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            11,209&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            11,86
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            49
+            <br></br>
+            Saturn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            9,449&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            29,46
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            52 <br></br>
+            Uranos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            4,007&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            84,01
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            27
+            <br></br>
+            Neptun&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            3,883&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            164,80
+            years&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            13
+            <br></br>
+          </pre>
+        </div>
       </div>
     </div>
   );
